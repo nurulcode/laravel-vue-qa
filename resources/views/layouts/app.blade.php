1bed4e157427ac24505d0a132500dce91392daa6
nurulcode <nurull.sarang@gmail.com>
2020-05-15 04:08:58 +0900

    Auth Using Policy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,6 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     @include('layouts._messages')
     @yield('footer')
 </body>
