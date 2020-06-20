9e2ec9ff504275b86ba3da5578201f462c031a72
nurulcode <nurull.sarang@gmail.com>
2020-06-20 18:54:41 +0900

    Markdown-it And Autosize Textarea

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="card text-center">
+    <div class="card">
         <div class="card-header">
             <ul class="nav nav-tabs card-header-tabs">
                 <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#write">Write</a>
+                    <a class="nav-link active" data-toggle="tab" href="#write"
+                        >Write</a
+                    >
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#preview">Preview</a>
+                    <a class="nav-link" data-toggle="tab" href="#preview"
+                        >Preview</a
+                    >
                 </li>
             </ul>
         </div>
@@ -14,14 +18,29 @@
             <div class="tab-pane active" id="write">
                 <slot></slot>
             </div>
-            <div class="tab-pane" id="preview">Priview...</div>
+            <div class="tab-pane" v-html="preview" id="preview"></div>
         </div>
     </div>
-
 </template>
 
 <script>
+import MarkdownIt from "markdown-it";
+import autosize from "autosize";
+
+const md = new MarkdownIt();
+
 export default {
-    props: ['body']
-}
+    props: ["body"],
+    computed: {
+        preview() {
+            return md.render(this.body);
+        }
+    },
+    mounted() {
+        autosize(this.$el.querySelector("textarea"));
+    },
+    updated() {
+        autosize(this.$el.querySelector("textarea"));
+    }
+};
 </script>
